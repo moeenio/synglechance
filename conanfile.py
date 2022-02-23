aee0d6054602c7a3bd1bce06df0b1b32ea9539fc
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-02-23 10:48:04 -0800

    do_copy_deps is only needed on Windows

--- conanfile.py
+++ conanfile.py
@@ -93,18 +93,19 @@ class MkxpConan(ConanFile):
 		self.do_copy_deps(self.copy_deps)
 
 	def do_copy_deps(self, copy):
-		deps = set(self.deps_cpp_info.deps) - set(
-			("cygwin_installer", "msys2_installer", "ruby_installer"))
-		for dep in deps:
-			copy("*.dll",
-				 dst="bin",
-				 src="bin",
-				 root_package=dep,
-				 keep_path=False)
-			copy("*.so*",
-				 dst="lib",
-				 src="lib",
-				 root_package=dep,
-				 keep_path=True)
-			if self.settings.build_type == "Debug":
-				copy("*.pdb", dst="bin", root_package=dep, keep_path=False)
+		if tools.os_info.is_windows:
+			deps = set(self.deps_cpp_info.deps) - set(
+				("cygwin_installer", "msys2_installer", "ruby_installer"))
+			for dep in deps:
+				copy("*.dll",
+					 dst="bin",
+					 src="bin",
+					 root_package=dep,
+					 keep_path=False)
+				copy("*.so*",
+					 dst="lib",
+					 src="lib",
+					 root_package=dep,
+					 keep_path=True)
+				if self.settings.build_type == "Debug":
+					copy("*.pdb", dst="bin", root_package=dep, keep_path=False)
