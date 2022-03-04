695d0bfec8d0052ed1f2a4b24f2e0185c201fe1d
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-03-03 23:41:58 -0800

    Fix ARM compilation selection

--- conanfile.py
+++ conanfile.py
@@ -70,14 +70,14 @@ class MkxpConan(ConanFile):
 
 	def build_configure(self):
 		cmake = CMake(self, msbuild_verbosity='minimal')
-		
+
 		if tools.os_info.is_macos:
 			cmake._generator = 'Xcode'
 		if self.options.platform == "steam":
 			cmake.definitions["STEAM"] = "ON"
 		if self.options.debug:
 			cmake.definitions["DEBUG"] = "ON"
-		cmake.definitions['ARCH'] = "arm64" if self.settings.arch == "armv8" else self.settings.arch
+		cmake.definitions['CMAKE_OSX_ARCHITEXTURES'] = "arm64" if self.settings.arch == "armv8" else self.settings.arch
 
 		cmake.configure()
 		cmake.build()
