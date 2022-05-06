5e3805249a9d4366a0f74e2eccaf89386fcde97b
Matthew Lyons <matthew@nowaffles.com>
2022-05-06 11:06:46 -0700

    Revert "Clean and sort out folders and files, update readme"
    This reverts commit 2769bfaf44b2c57480841cbb731a2468c35c727e.

---
+++ make-linux.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -e
+
+cd `dirname $0`
+
+# User-configurable variables
+linux_version="0.1.0"
+with_steamshim=${STEAM:-true}
+debug=${DEBUG:-false}
+
+# Colors
+white="\033[0;37m"      # White - Regular
+bold="\033[1;37m"       # White - Bold
+cyan="\033[1;36m"       # Cyan - Bold
+green="\033[1;32m"      # Green - Bold
+color_reset="\033[0m"   # Reset Colors
+
+
+
+echo -e "${white}Compiling ${bold}SyngleChance v${linux_version} ${white}engine for Linux...${color_reset}\n"
+
+mkdir -p build
+cd build
+
+# Generate makefile and build main
+echo "-> ${cyan}Install dependencies...${color_reset}"
+conan install .. --build=missing -o platform=$([ $with_steamshim == true ] && echo "steam" || echo "standalone") -o debug=$([ $debug == true ] && echo "True" || echo "False") -s arch=x86_64
+echo "-> ${cyan}Compile engine...${color_reset}"
+conan build ..
+
+# Build journal
+echo -e "-> ${cyan}Compile journal...${color_reset}"
+cd ..
+pyinstaller journal/unix/journal.spec --windowed
+
+echo -e "\n${green}Complete!  ${white}Please report any issues to https://github.com/GooborgStudios/synglechance/issues${color_reset}"
